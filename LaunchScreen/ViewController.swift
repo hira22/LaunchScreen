9d358f349f71b3759ee8694fb79e0524420f29e8
hira22 <masayuki.hiraoka@ietty.co.jp>
2020-01-10 11:22:36 +0900

    Initial Commit

---
+++ LaunchScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaunchScreen
+//
+//  Created by hiraoka on 2020/01/10.
+//  Copyright © 2020 hiraoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
